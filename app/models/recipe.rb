5a5c1aa0803b52e9218f28afae1747947baa28be
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-10-09 18:32:57 -0500

    Add validations to the Recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,10 @@
 class Recipe < ApplicationRecord
   belongs_to :user
+
+  # Validations.
+  validates :name, presence: true, length: { maximum: 250 }
+  validates :preparation_time, presence: true, comparison: { greater_than_or_equal_to: 0 }, numericality: { only_integer: true }
+  validates :cooking_time, presence: true, comparison: { greater_than_or_equal_to: 0 }, numericality: { only_integer: true }
+  validates :description, presence: true
+  validates :public, inclusion: { in: [true, false] }
 end
