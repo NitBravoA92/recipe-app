2d85f62b849d22926280608887f43bf5b2a88781
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-10-09 14:16:28 -0500

    Added letter-opener configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,6 +61,9 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+	config.server_timing = true
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
