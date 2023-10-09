dee6fcb4749fc9aec9befe4deb6cd64015618972
Carmen Bravo <nbravoalcala@gmail.com>
2023-10-09 16:01:23 -0400

    Generate the foods migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_09_184248) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_09_195435) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "foods", force: :cascade do |t|
+    t.string "name"
+    t.string "measurement_unit"
+    t.decimal "price", precision: 10, scale: 2
+    t.integer "quantity"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_foods_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
@@ -32,4 +43,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_09_184248) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "foods", "users"
 end
