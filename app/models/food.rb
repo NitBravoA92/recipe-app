46efd5cd60a5db44977677c9f1a1d2cc2f18cd38
Carmen Bravo <nbravoalcala@gmail.com>
2023-10-09 19:52:07 -0400

    Fix linters errors

--- app/models/food.rb
+++ app/models/food.rb
@@ -6,5 +6,4 @@ class Food < ApplicationRecord
   validates :name, :measurement_unit, :price, :quantity, presence: true
   validates :name, length: { maximum: 150 }
   validates :quantity, numericality: { greater_than_or_equal_to: 0 }
-
 end
--- spec/requests/foods_spec.rb
+++ spec/requests/foods_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "Foods", type: :request do
+RSpec.describe 'Foods', type: :request do
   # describe "GET /index" do
   #   it "returns http success" do
   #     get "/foods/index"
