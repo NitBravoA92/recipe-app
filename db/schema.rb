e0502a03928b9773d17f2b55d081c984a53130e9
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-10-10 15:32:09 -0500

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_09_204948) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_10_193737) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,7 +26,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_09_204948) do
   end
 
   create_table "recipe_foods", force: :cascade do |t|
-    t.string "quantity"
+    t.integer "quantity", default: 0
     t.bigint "recipe_id", null: false
     t.bigint "food_id", null: false
     t.datetime "created_at", null: false
