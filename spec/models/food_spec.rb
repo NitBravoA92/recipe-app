d2ff3ed5a11acb7eef210559dbd086c087730c1e
Carmen Bravo <nbravoalcala@gmail.com>
2023-10-10 19:09:57 -0400

    Create tests to check the Food model is valid and a food belongs to Gerard user

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Food, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:food) { create(:food) }
+
+  describe "food" do
+    it "should be valid" do
+      expect(food).to be_valid
+    end
+
+    it "should be owned by Gerard" do
+      expect(food.user.name).to eq("Gerard")
+    end
+  end
 end
