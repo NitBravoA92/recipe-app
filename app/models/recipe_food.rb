f385999280e1b8cf0aa94d4f785b329abf37417c
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-10-10 16:02:50 -0500

    Modify the increment_quantity method

--- app/models/recipe_food.rb
+++ app/models/recipe_food.rb
@@ -1,4 +1,12 @@
 class RecipeFood < ApplicationRecord
   belongs_to :recipe, class_name: 'Recipe'
   belongs_to :food, class_name: 'Food'
+  after_create :increment_quantity
+
+  def increment_quantity
+		recipe_foods = RecipeFood.where(recipe_id: recipe_id)
+		recipe_foods.each do |recipe_food|
+			recipe_food.increment!(:quantity)
+		end
+  end
 end
