18911865a8ed3728f7b83b9f0454abf2ca10b14e
Carmen Bravo <nbravoalcala@gmail.com>
2023-10-10 19:07:30 -0400

    Create a basic test to validate the User model is valid

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:user) { create(:user) }
+
+  describe "user" do
+    it "should be valid" do
+      expect(user).to be_valid
+    end
+  end
 end
