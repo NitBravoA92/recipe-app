688447a2aaf5f3a7ebdd13e238bdeeb397ea1a09
Carmen Bravo <nbravoalcala@gmail.com>
2023-10-10 19:19:35 -0400

    Create test to validate the GET /foods request in the Foods controller - index action

--- spec/requests/foods_spec.rb
+++ spec/requests/foods_spec.rb
@@ -1,10 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe 'Foods', type: :request do
-  # describe "GET /index" do
-  #   it "returns http success" do
-  #     get "/foods/index"
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
+
+  # request to: /foods
+  describe 'GET /index' do
+    # the user Gerard logged in before run the tests
+    before do
+      user = FactoryBot.create(:user)
+      food = FactoryBot.create(:food, user: user)
+      sign_in user
+      get foods_path
+    end
+
+    # test if the response status was correct (status 200)
+    it 'returns http success' do
+      expect(response).to have_http_status(:success)
+    end
+
+    # test if a correct template was rendered.
+    it 'renders the index template' do
+      expect(response).to render_template(:index)
+    end
+    
+    # test If the response body includes correct content.
+    it 'renders the index template with correct content' do
+      expected_result = 'Foods List'
+      expect(response.body).to include(expected_result)
+
+      expected_result_food = 'Apple'
+      expect(response.body).to include(expected_result_food)
+    end
+  end
 end
