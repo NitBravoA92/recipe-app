95b2d0cc89e835da58328295257be79bdfdf9380
Carmen Bravo <nbravoalcala@gmail.com>
2023-10-10 19:08:01 -0400

    Create test cases to validate the attributes of the User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,4 +9,18 @@ RSpec.describe User, type: :model do
       expect(user).to be_valid
     end
   end
+
+  describe "attributes" do
+    it 'User object should have a name attribute, with a valid value' do
+      expect(user.name).to eq('Gerard')
+    end
+
+    it 'User object should have an email attribute, with a valid value' do
+      expect(user.email).to eq('gerard123@domain.com')
+    end
+
+    it 'User object should have a password attribute, with a valid value' do
+      expect(user.password).to eq('gerard12345')
+    end
+  end
 end
