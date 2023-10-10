69e6ec3087fdb07ee5d3190b1faed275b6b3c167
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-10-10 17:20:58 -0500

    Add a validation to avoid duplicated ingredients in a recipe

--- app/models/recipe_food.rb
+++ app/models/recipe_food.rb
@@ -1,12 +1,7 @@
 class RecipeFood < ApplicationRecord
   belongs_to :recipe, class_name: 'Recipe'
   belongs_to :food, class_name: 'Food'
-  after_create :increment_quantity
 
-  def increment_quantity
-    recipe_foods = RecipeFood.where(recipe_id:)
-    recipe_foods.each do |recipe_food|
-      recipe_food.increment!(:quantity)
-    end
-  end
+  # Avoids duplicated ingredients.
+	validates :food_id, uniqueness: true
 end
