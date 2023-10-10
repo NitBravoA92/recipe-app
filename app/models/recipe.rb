3e603bcdff32c46740c693c935c4f875b90fb0f6
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-10-10 17:11:23 -0500

    Create a method to count the number of ingredients in a recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -11,6 +11,10 @@ class Recipe < ApplicationRecord
   validates :public, inclusion: { in: [true, false] }
 
   def total_price
-    recipe_foods.sum { |recipe_food| recipe_food.food.price * recipe_food.food.quantity }
+    foods.sum { |recipe_food| recipe_food.price * recipe_food.quantity }
+  end
+
+  def count_ingredients
+  	foods.count
   end
 end
