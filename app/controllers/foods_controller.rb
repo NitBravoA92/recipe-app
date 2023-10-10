be4c0c40a420f9492343424e79a51549f849fe4c
Carmen Bravo <nbravoalcala@gmail.com>
2023-10-09 23:57:03 -0400

    Add the create action in the foods controller to save a new food in the database

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -8,7 +8,18 @@ class FoodsController < ApplicationController
     @food = @food.new
   end
 
+  def create
+    food = @food.new(food_params)
+    render :new unless food.save
+    flash[:notice] = 'The Food was created successfully!'
+    redirect_to foods_path
+  end
+
   private
+
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
+  end
 
   def food_by_user
     @food = current_user.foods
