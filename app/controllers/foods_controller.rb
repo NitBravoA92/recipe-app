55b0ad48ffd70c0e22098cfb81b90e66f6c71c24
Carmen Bravo <nbravoalcala@gmail.com>
2023-10-09 23:54:12 -0400

    Create the new action and the food_by_user method in the foods controller

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -3,4 +3,14 @@ class FoodsController < ApplicationController
   def index
     @foods = current_user.foods.all
   end
+
+  def new
+    @food = @food.new
+  end
+
+  private
+
+  def food_by_user
+    @food = current_user.foods
+  end
 end
