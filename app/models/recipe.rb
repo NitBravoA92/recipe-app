40e45bab618a230a2af02667bad89fd1b164fe7d
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-10-09 19:50:38 -0500

    Modified the validations in the Recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,9 +2,8 @@ class Recipe < ApplicationRecord
   belongs_to :user
 
   # Validations.
-  validates :name, presence: true, length: { maximum: 250 }
-  validates :preparation_time, presence: true, comparison: { greater_than_or_equal_to: 0 }, numericality: { only_integer: true }
-  validates :cooking_time, presence: true, comparison: { greater_than_or_equal_to: 0 }, numericality: { only_integer: true }
-  validates :description, presence: true
+  validates :name, :description, presence: true
+  validates :name, length: { maximum: 250 }
+  validates :preparation_time, :cooking_time, presence: true, comparison: { greater_than_or_equal_to: 0 }, numericality: { only_integer: true }
   validates :public, inclusion: { in: [true, false] }
 end
