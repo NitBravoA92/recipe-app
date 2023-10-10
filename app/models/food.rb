fc82bff9b2520787b0774cd22e5f68522515d5ef
Carmen Bravo <nbravoalcala@gmail.com>
2023-10-10 12:14:53 -0400

    Add the join table name in the food's model association

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,6 +1,6 @@
 class Food < ApplicationRecord
   belongs_to :user
-  has_and_belongs_to_many :recipes
+  has_and_belongs_to_many :recipes, join_table: 'recipe_foods'
 
   # validations
   validates :name, :measurement_unit, :price, :quantity, presence: true
