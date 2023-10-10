88c90065f6010b93773d4139778e0d77576d6906
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-10-10 15:46:06 -0500

    Add the relationship with the recipe foods

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,7 @@
 class Recipe < ApplicationRecord
   belongs_to :user
   has_and_belongs_to_many :foods, join_table: 'recipe_foods'
+  has_many :recipe_foods
 
   # Validations.
   validates :name, :description, presence: true
