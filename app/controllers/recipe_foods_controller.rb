9fbfa518cd7832e27c46a9ebb14a4def3e5c83bf
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-10-11 17:56:51 -0500

    Add actions new and create in the recipe foods controller

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -1,5 +1,28 @@
 class RecipeFoodsController < ApplicationController
+  before_action :foods_by_recipe
   def index
     @recipes = Recipe.includes(:recipe_foods, :user).where(public: true).order(created_at: :desc)
+  end
+
+  def new
+  	@recipe_food = @recipe_food.new
+  end
+
+  def create
+		recipe_food = @recipe_food.new(recipe_food_params)
+		render :new unless recipe_food.save
+		flash[:notice] = 'The ingredient was added successfully!'
+    redirect_to recipe_path(@recipe)
+	end
+
+  private
+
+  def recipe_food_params
+  	params.require(:recipe_food).permit(:food_id, :quantity)
+  end
+
+  def foods_by_recipe
+  	@recipe = Recipe.find(params[:recipe_id])
+		@recipe_food = @recipe.recipe_foods
   end
 end
