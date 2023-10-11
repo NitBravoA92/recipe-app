1bb51139fc829c5ebe2ad86a0f966f6a9a9216ad
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-10-10 19:36:09 -0500

    Fixed linters

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,15 +1,15 @@
 class RecipesController < ApplicationController
-	before_action :recipe_by_user
+  before_action :recipe_by_user
   def index
     @recipes = current_user.recipes.all
   end
 
   def new
-  	@recipe = @recipe.new
+    @recipe = @recipe.new
   end
 
   def create
-  	recipe = @recipe.new(recipe_params)
+    recipe = @recipe.new(recipe_params)
     render :new unless recipe.save
     flash[:notice] = 'The Recipe was created successfully!'
     redirect_to recipes_path
@@ -18,11 +18,11 @@ class RecipesController < ApplicationController
   private
 
   def recipe_params
-  	# Convert the hours into minutes.
+    # Convert the hours into minutes.
     params[:recipe][:preparation_time] = (params[:recipe][:preparation_time].to_f * 60).to_i
     params[:recipe][:cooking_time] = (params[:recipe][:cooking_time].to_f * 60).to_i
 
-    #Require parameters
+    # Require parameters
     params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public)
   end
 
