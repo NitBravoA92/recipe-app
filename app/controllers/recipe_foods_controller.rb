935c164e5f6823c3eb0a11b54b8da0509f378dbe
Carmen Bravo <nbravoalcala@gmail.com>
2023-10-11 17:52:42 -0400

    Create the destroy action in RecipeFoodsController to remove a recipe's food from the list

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -1,5 +1,18 @@
 class RecipeFoodsController < ApplicationController
+  before_action :authenticate_user!, only: %i[destroy]
+
   def index
     @recipes = Recipe.includes(:recipe_foods, :user).where(public: true).order(created_at: :desc)
+  end
+
+  def destroy
+    recipe_food = RecipeFood.find(params[:id])
+    recipe = recipe_food.recipe
+    if recipe_food.destroy
+      flash[:notice] = 'The Food was remove successfully!'
+    else
+      flash[:alert] = 'The Food was not remove!'
+    end
+    redirect_to recipe_path(recipe)
   end
 end
