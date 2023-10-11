0fe9ece50a83eb767fd63f70d841b8675d1126be
Carmen Bravo <nbravoalcala@gmail.com>
2023-10-11 17:42:04 -0400

    Create the get_total_price method in the recipe_food model

--- app/models/recipe_food.rb
+++ app/models/recipe_food.rb
@@ -4,4 +4,9 @@ class RecipeFood < ApplicationRecord
 
   # Avoids duplicated ingredients.
   validates :food_id, uniqueness: true
+
+  #methods
+  def get_total_price
+    (self.quantity * food.price).round(2)
+  end
 end
