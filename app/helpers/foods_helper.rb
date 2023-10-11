e3d67ff4934c013482c683671376580686164e6d
Carmen Bravo <nbravoalcala@gmail.com>
2023-10-11 17:44:34 -0400

    Create the helper function: get_measurement_prefix to format the measurement value

--- app/helpers/foods_helper.rb
+++ app/helpers/foods_helper.rb
@@ -1,2 +1,10 @@
 module FoodsHelper
+  def get_measurement_prefix(measurement)
+    options = {
+      'grams' => 'g',
+      'milliliters' => 'ml',
+      'units' => 'units',
+    }
+    options[measurement]
+  end
 end
