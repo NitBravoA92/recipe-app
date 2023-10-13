6dc7ff55887d5937335c7794ea57ddbb7ad877cc
Carmen Bravo <nbravoalcala@gmail.com>
2023-10-13 00:26:49 -0400

    Create tests to validate all the validations in the RecipeFood model

--- spec/models/recipe_food_spec.rb
+++ spec/models/recipe_food_spec.rb
@@ -8,4 +8,16 @@ RSpec.describe RecipeFood, type: :model do
       expect(recipe_food).to be_valid
     end
   end
+
+  describe 'recipe food validations' do
+    let(:recipe) { create(:recipe) }
+    let(:food) { create(:food) }
+
+    it 'the food same food should be not be present twice in the same recipe' do
+      first_recipe_food = RecipeFood.create(food: food, recipe: recipe, quantity: 5)
+      second_recipe_food = RecipeFood.create(food: food, recipe: recipe, quantity: 8) 
+
+      expect(second_recipe_food).to_not be_valid
+    end
+  end
 end
