a0345526c0a42a7fbabaa5a796ba17140cae1988
Carmen Bravo <nbravoalcala@gmail.com>
2023-10-13 12:37:48 -0400

    Add the rules to destroy recipe_foods records in cascade

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,7 +2,7 @@ class Recipe < ApplicationRecord
   # before_validation :convert_hours_to_minutes
   belongs_to :user
   has_and_belongs_to_many :foods, join_table: 'recipe_foods'
-  has_many :recipe_foods
+  has_many :recipe_foods, dependent: :destroy
 
   # Validations.
   validates :name, :description, presence: true
