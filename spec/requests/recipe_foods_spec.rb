85b53d38ae3f1e8f291501ef3fb6ed9aa4197480
Carmen Bravo <nbravoalcala@gmail.com>
2023-10-13 00:54:17 -0400

    Create the test to validate the GET /public_recipes request

--- spec/requests/recipe_foods_spec.rb
+++ spec/requests/recipe_foods_spec.rb
@@ -1,7 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe 'RecipeFoods', type: :request do
-  # describe 'GET /index' do
-  #   pending "add some examples (or delete) #{__FILE__}"
-  # end
+  describe 'GET /public_recipes' do
+    # the user should not be logged in
+    before do
+      user = FactoryBot.create(:user)
+      @recipe = FactoryBot.create(:recipe, user:)
+      get public_recipes_path
+    end
+
+    # test if the response status was correct (status 200)
+    it 'returns http success' do
+      expect(response).to have_http_status(:success)
+    end
+
+    # test if a correct template was rendered.
+    it 'renders the index template' do
+      expect(response).to render_template(:index)
+    end
+
+    # test If the response body includes correct content.
+    it 'renders the index template with correct content' do
+      expected_result = 'Public Recipes'
+      expect(response.body).to include(expected_result)
+
+      expected_result_recipe = 'Cookies'
+      expect(response.body).to include(expected_result_recipe)
+    end
+  end
 end
