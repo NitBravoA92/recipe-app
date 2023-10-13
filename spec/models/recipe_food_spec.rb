a0d544a592a09661409d40b14fc234d27714762a
Carmen Bravo <nbravoalcala@gmail.com>
2023-10-13 00:25:47 -0400

    Create the initial test to validate the recipe_food model instance

--- spec/models/recipe_food_spec.rb
+++ spec/models/recipe_food_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
-# RSpec.describe RecipeFood, type: :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+RSpec.describe RecipeFood, type: :model do
+  let(:recipe_food) { create(:recipe_food) }
+
+  describe 'recipe food' do
+    it 'should be valid' do
+      expect(recipe_food).to be_valid
+    end
+  end
+end
