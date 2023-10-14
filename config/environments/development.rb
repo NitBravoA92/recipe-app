c7ec30b2913d8a53e6fdd34bc39e8a830b4da905
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-10-14 18:04:07 -0500

    Remove a line of code

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -46,13 +46,11 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
 	config.server_timing = true
-
-  config.action_mailer.delivery_method = :letter_opener
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
